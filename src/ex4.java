79a09f84b671c2a08babd0fe2680d6a9ce7c3dfe
TaunoPoldoja <tauno.poldoja@tptlive.ee>
2016-11-13 22:59:51 +0200

    Initial commit

---
+++ src/ex4.java
@@ -0,0 +1,17 @@
+/**
+ * Created by tauno on 13.11.2016.
+ */
+public class ex4  {
+
+    public static void zool(String name, String street, int value) {
+        System.out.print(value + " ");
+        System.out.print(name + " ");
+        System.out.print(street + " ");
+
+    }
+
+    public static void main(String[] args) {
+        zool("koer", "Kivi", 11);
+        System.out.println();
+    }
+}
